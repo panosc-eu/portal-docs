53b609a8ae1de0a2befc73c06a0167e7623ecd60
kirienko <yury.kirienko@gmail.com>
2021-05-27 16:44:21 +0200

    install nodejs and portal components

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ else [ -f '/etc/redhat-release' ]
   DISTRO="Redhat"
 fi
 
+CUBE_NAMESPACE="panosc-portal"
+
+
 # TODO: if Ubuntu/Debian
 
 # helm
@@ -39,6 +42,54 @@ echo "Downloading $KEYCLOAK:"
 curl -LO "https://github.com/keycloak/keycloak/releases/download/$KEYCLOAK_VER/$KEYCLOAK.tar.gz"
 tar xvzf "$KEYCLOAK.tar.gz"
 
-# Configure Keycloak
-./$KEYCLOAK/bin/add-user.sh
+# TODO: Configure Keycloak
+#./$KEYCLOAK/bin/add-user.sh
 #./$KEYCLOAK/bin$ ./standalone.sh
+
+
+sudo apt-get install -y socat git
+
+# Install the portal components
+git clone https://github.com/panosc-portal/api-service-client-cli
+git clone https://github.com/panosc-portal/api-service
+git clone https://github.com/panosc-portal/account-service-client-cli
+git clone https://github.com/panosc-portal/cloud-service-client-cli
+git clone https://github.com/panosc-portal/cloud-service
+git clone https://github.com/panosc-portal/cloud-provider-client-cli
+git clone https://github.com/panosc-portal/frontend
+git clone https://github.com/panosc-portal/account-service
+git clone https://github.com/panosc-portal/cloud-provider-kubernetes
+git clone https://github.com/panosc-portal/simple-notebook-client
+git clone https://github.com/panosc-portal/desktop-service-web-test-client
+git clone https://github.com/panosc-portal/desktop-service
+git clone https://github.com/panosc-portal/microservices-integration-test
+git clone https://github.com/panosc-portal/helm-charts
+git clone https://github.com/panosc-portal/remote-desktop-test-image
+git clone https://github.com/panosc-portal/notebook-service-web-test-client
+git clone https://github.com/panosc-portal/notebook-service
+
+
+# Install helm components:
+helm repo add panosc-portal https://panosc-portal.github.io/helm-charts/
+helm repo update
+
+kubectl create namespace $CUBE_NAMESPACE
+# TODO: install helm components:
+
+
+# Install Node
+if [ $DISTRO == 'ubuntu' ]; then
+  curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
+  sudo apt-get install -y nodejs npm
+else
+  curl -fsSL https://rpm.nodesource.com/setup_15.x | bash -
+  # remove an version if any:
+  sudo yum remove nodejs -y
+  sudo yum install nodejs npm -y
+fi
+
+
+### account-service-client-cli
+# Go to the `api-service-client-cli` repo and install Node components:
+cd api-service-client-cli
+npm install && npm audit fix
